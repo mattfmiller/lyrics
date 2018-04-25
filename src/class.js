fff53cd4f9f95a1c29a33cd869730fd3e0251d28
Matt Miller and William Chu <william@>
2018-04-25 14:48:58 -0700

    create lyrics API call and user interface logic and html.

--- src/class.js
+++ src/class.js
@@ -1,11 +1,29 @@
-class Class {
-  constructor(parameter) {
-    this.prop = parameter;
-  }
+class Lyrics {
 
-  method() {
-    this.prop = value;
+  apiCall(artist, song, displayData, displayError) {
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      // let error;
+      let url =`https://api.lyrics.ovh/v1/${artist}/${song}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          // error = new Error(request.statusText);
+          reject();
+        }
+      }
+      request.open("GET", url);
+      request.send();
+    });
+
+    promise.then(function(response) {
+      let result = JSON.parse(response);
+      displayData(artist, song, result);
+    }, function() {
+      displayError();
+    });
   }
 }
 
-export { Class };
+export { Lyrics };
--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
-// import { function } from './class';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './styles.css';
-// import $ from 'jquery';
-//
-// $(document).ready(function() {
-//
-// });
+import { Lyrics } from './class.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import $ from 'jquery';
+
+let displayData = function(inputArtist, inputSong, response) {
+  $("#lyrics").empty()&&$("#errors").empty();
+  $("#lyrics").append(`${inputArtist} - ${inputSong}<br>`);
+  $("#lyrics").append(response.lyrics)
+}
+
+let displayError = function() {
+  $("#lyrics").empty();
+  $("#errors").text(`There was an error processing your request: No lyrics found. Please try again.`)
+}
+
+$(document).ready(function() {
+  $("#button").click(function(){
+    let lyrics = new Lyrics();
+    let inputArtist = $("#artist").val();
+    let inputSong = $("#song").val();
+    console.log(inputArtist + " " + inputSong);
+    lyrics.apiCall(inputArtist, inputSong, displayData, displayError);
+  });
+});
